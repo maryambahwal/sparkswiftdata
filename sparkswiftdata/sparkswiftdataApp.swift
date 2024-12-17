208677b83419dd64bbb228e81a6db69b8716f25a
Maryam Bahwal <mrim2m7md@gmail.com>
2024-12-17 11:46:05 +0300

    Initial Commit

---
+++ sparkswiftdata/sparkswiftdataApp.swift
@@ -0,0 +1,17 @@
+//
+//  sparkswiftdataApp.swift
+//  sparkswiftdata
+//
+//  Created by Maryam Bahwal on 16/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct sparkswiftdataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ sparkswiftdataUITests/sparkswiftdataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  sparkswiftdataUITestsLaunchTests.swift
+//  sparkswiftdataUITests
+//
+//  Created by Maryam Bahwal on 16/06/1446 AH.
+//
+
+import XCTest
+
+final class sparkswiftdataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
